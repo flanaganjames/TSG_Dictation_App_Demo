ececf52c7e9084fdaf12186a5cc8e250c1454857
jeffrey-l-copeland <jeff.copeland@gmail.com>
2014-09-11 16:01:49 -0700

    add logic to parse vital signs, and validate them; now need to refactor the warning box a little to allow multiple warnings

--- SLC/SLC/parser.h
+++ SLC/SLC/parser.h
@@ -14,5 +14,10 @@ extern list<char *> _rec_hpi, _rec_exam;
 extern list<char *> _all_complete, _comp_req, _comp_rec;
 	// resource links
 extern list<char *> _links;
+	// vital signs -- one item per category
+extern char *VS_p, *VS_r, *VS_t, *VS_sbp, *VS_dbp;
+	// vital sign values
+extern int VVS_p, VVS_r, VVS_sbp, VVS_dbp;
+extern float VVS_t;
 	// possible information on differential diagnosis
 extern char *differential;

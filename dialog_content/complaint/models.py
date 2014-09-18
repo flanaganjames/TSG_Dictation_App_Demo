f605d1d74c48a02fa4cb85320a76640348bdfa5c
Marshall Smith <marshall.ds@gmail.com>
2014-09-18 08:35:19 -0600

    setup django project and models to generate sqlite

---
+++ dialog_content/complaint/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class ComplaintGroup(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+
+    def __unicode__(self):
+        return "ComplaintGroup: %s" % self.name
+
+
+class Complaint(models.Model):
+    name = models.CharField(max_length=250)
+    complaint_group = models.ForeignKey(ComplaintGroup)
+
+    def __unicode__(self):
+        return "Complaint: %s" % self.name

bb62d0d8df03d6259baccbfc4a7277b8df7c565d
Bryan <bryan@newlinetechnicalinnovations.com>
2014-09-18 10:08:48 -0600

    Add in experiment for reading text from external applications.

---
+++ ReadWriteTextTest/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ReadWriteTextTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.countdown = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read UI Elements";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // countdown
+            // 
+            this.countdown.AutoSize = true;
+            this.countdown.Location = new System.Drawing.Point(462, 22);
+            this.countdown.Name = "countdown";
+            this.countdown.Size = new System.Drawing.Size(0, 13);
+            this.countdown.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(760, 509);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.countdown);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Read Text From External Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label countdown;
+        private System.Windows.Forms.RichTextBox textBox1;
+    }
+}
+

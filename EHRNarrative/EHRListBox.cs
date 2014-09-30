de07f5ac2248296a21fe52753b85b237038db3c9
Marshall Smith <marshall@newline.us>
2014-09-30 11:15:52 -0600

    Selected: option A for #44

--- EHRNarrative/EHRListBox.cs
+++ EHRNarrative/EHRListBox.cs
@@ -25,16 +25,19 @@ namespace EHRNarrative
 
         public void drawItem(DrawItemEventArgs e, Padding margin, Font font, StringFormat aligment)
         {
-
+            Brush textColor = Brushes.Black;
             // if selected, mark the background differently
             if (this.Element.selected == "present")
             {
-                e.Graphics.FillRectangle(Brushes.LightGoldenrodYellow, e.Bounds);
+                e.Graphics.FillRectangle(Brushes.SteelBlue, e.Bounds);
+                textColor = Brushes.White;
+                
             }
             else if (this.Element.selected == "not present")
             {
-                //e.Graphics.FillRectangle(Brushes.LightSalmon, e.Bounds);
-                e.Graphics.DrawLine(Pens.Black, e.Bounds.X, e.Bounds.Y + e.Bounds.Height, e.Bounds.X + e.Bounds.Width, e.Bounds.Y);
+                //e.Graphics.FillRectangle(Brushes.LightGray, e.Bounds);
+                e.Graphics.DrawLine(Pens.SlateGray, e.Bounds.X, e.Bounds.Y + e.Bounds.Height, e.Bounds.X + e.Bounds.Width, e.Bounds.Y);
+                textColor = Brushes.DarkSlateGray;
             }
             else
             {
@@ -51,7 +54,7 @@ namespace EHRNarrative
                                                  (int)font.GetHeight() + 2);
 
             // draw the text within the bounds
-            e.Graphics.DrawString(this.Element.Name, font, Brushes.Black, textBounds, aligment);
+            e.Graphics.DrawString(this.Element.Name, font, textColor, textBounds, aligment);
 
             // put some focus rectangle
             e.DrawFocusRectangle();

a9167381b4cf29e4ee50beb84892c83cd2e7c7e9
Marshall Smith <marshall@newline.us>
2014-10-09 16:52:15 -0600

    insert text into EHRN and send SLC command when appropriate. Closes #49

--- EHRNarrative/ExamDialog.cs
+++ EHRNarrative/ExamDialog.cs
@@ -262,6 +262,14 @@ namespace EHRNarrative
                 narrative_window.ReplaceKeyword("[" + keyword + "]/" + EHRString);
                 narrative_window.ReplaceKeyword("[\\cf2 " + keyword + "\\cf1 ]/" + EHRString);
             }
+
+            foreach (String EHR_text in data.elements
+                .Where(x => x.selected != null && x.EHR_replace != null)
+                .Select(x => x.EHR_replace).Distinct()
+                )
+            {
+                narrative_window.ReplaceKeyword(EHR_text);
+            }
         }
 
         private void UpdateSLC()
@@ -271,7 +279,19 @@ namespace EHRNarrative
             {
                 groupData.Add("dataqual " + data.dialog.Name + " " + group.Name + " " + group.SelectedItemCount(data).ToString());
             }
+
+
+            var blah = data.elements.Where(x => x.SLC_command != null);
+            foreach (String SLC_command in data.elements
+                .Where(x => x.selected != null && x.SLC_command != null)
+                .Select(x => x.SLC_command).Distinct()
+                )
+            {
+                groupData.Add(SLC_command);
+            }
+
             narrative_window.NotifySLC(String.Join(" ! ", groupData));
+
         }
 
         private void DoneButton_Click(object sender, EventArgs e)
